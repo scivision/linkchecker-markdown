dc5093055436cdb22553cbb83ab10ac08f4345bf
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-01 23:11:10 -0400

    mypy glitch

--- src/linkcheckmd/__init__.py
+++ src/linkcheckmd/__init__.py
@@ -4,4 +4,4 @@ import asyncio
 from .base import check_remotes, check_local
 
 if os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore

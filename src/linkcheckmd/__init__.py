9e873bd83665bd6da9b0f02541377cc8db9a3a18
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-17 08:34:38 -0500

    anno

--- src/linkcheckmd/__init__.py
+++ src/linkcheckmd/__init__.py
@@ -4,4 +4,5 @@ import asyncio
 from .base import check_links, check_local, check_remotes
 
 if os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore
+# type ignore needed for non-windows mypy
